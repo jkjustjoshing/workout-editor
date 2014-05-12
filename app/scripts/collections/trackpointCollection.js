bdf51ce71b5a17f742f638ae33304f3ea0746d98
Josh Kramer <josh@joshkra.me>
2014-05-11 23:02:19 -0400

    Calculate pace data and show it

--- app/scripts/collections/trackpointCollection.js
+++ app/scripts/collections/trackpointCollection.js
@@ -6,5 +6,28 @@ window.WorkoutEditor.Collections.TrackpointCollection = Backbone.Collection.exte
     		distance += this.at(i).distance(this.at(i+1));
     	}
     	return distance;
+    },
+    paces: function() {
+    	var paces = [],
+    	    lapStart = this.at(0).get('time'),
+    	    lapDistance = 0;
+    	    distance = 0;
+    	for(var i = 0; i < this.length-1; ++i) {
+    		var thisDist = this.at(i).distance(this.at(i+1));
+    		distance += thisDist;
+    		if(distance >= lapDistance + 1) {
+    			lapDistance++;
+    			paces.push({
+    				mile: lapDistance,
+    				startTime: lapStart,
+    				endTime: this.at(i+1).get('time'),
+    				distance: 1,
+    				elapsed: this.at(i+1).get('time').diff(lapStart) / 1000
+    			});
+    			lapStart = this.at(i+1).get('time');
+    		}
+    	}
+    	return paces;
     }
+
 });
