64a0fb96767b279aafd73357095cac612b77c4c2
Josh Kramer <jkra@bn.co>
2015-06-21 11:04:57 -0400

    Fix memory leak

--- app/scripts/directives/input_type_file.js
+++ app/scripts/directives/input_type_file.js
@@ -8,14 +8,20 @@ angular.module('workoutEditorApp')
         	ngModel: '='
         },
         link: function (scope, element, attr, ngModel) {
-          if (attr.type !== 'file') return;
+          if (attr.type !== 'file') {
+            return;
+          }
 
           // Override the input event and add custom 'path' logic
-          element.unbind('change');
-          element.bind('change', function () {
+          element.off('change');
+          element.on('change', function () {
             scope.$apply(function() {
-            	scope.ngModel = event.target.files[0]
+            	scope.ngModel = event.target.files[0];
           	}.bind(this));
+          });
+
+          scope.$on('$destroy', function () {
+            element.off('change');
           });
         }
     };
