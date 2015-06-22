92879913817682fdd40d7868c6b6ba924064f131
Josh Kramer <jkra@bn.co>
2015-06-22 09:12:08 -0400

    Make jk-google-map a component

--- app/scripts/directives/jkGoogleMap.js
+++ app/scripts/directives/jkGoogleMap.js
@@ -3,33 +3,33 @@
 angular.module('workoutEditorApp')
   .directive('jkGoogleMap', function (Trackpoints) {
     return {
-      restrict: 'A',
+      restrict: 'E',
       scope: {
         passedOptions: '&'
       },
       link: function (scope, element, attr, ngModel) {
-  
+
         var defaults = {
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-   
+
         scope.options = angular.extend({}, defaults, scope.passedOptions);
-    
-  
+
+
         var initial = true;
         scope.$watch(function() {return Trackpoints.getList();}, function(newVal) {
           if(newVal && newVal.length && initial) {
             initial = false;
-  
+
             var googleCoordinates = [];
-  
+
             newVal.forEach(function(dataPoint) {
               googleCoordinates.push(dataPoint.getGoogleLatLng());
             });
-      
+
             var map = new google.maps.Map(element[0], scope.options);
-  
-  
+
+
             var flightPath = new google.maps.Polyline({
               path: googleCoordinates,
               geodesic: true,
@@ -39,16 +39,16 @@ angular.module('workoutEditorApp')
             });
 
             flightPath.setMap(map);
-       
+
             var bounds = new google.maps.LatLngBounds();
             bounds.extend(Trackpoints.getNorthEast().getGoogleLatLng());
             bounds.extend(Trackpoints.getSouthWest().getGoogleLatLng());
 
             map.fitBounds(bounds);
-       
+
           }
         });
-  
+
       }
     };
   });
